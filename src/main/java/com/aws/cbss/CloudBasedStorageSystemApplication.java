816f36e43b5e6f6fceedbf0f8810dc1a9600bf88
Sathish-Kumar-E <sathishkumar13121998@gmail.com>
2025-03-31 12:30:37 -0400

    initial Commit

---
+++ src/main/java/com/aws/cbss/CloudBasedStorageSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aws.cbss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBasedStorageSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBasedStorageSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/cbss/CloudBasedStorageSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.cbss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudBasedStorageSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
